e612443bc6dd4ecba70c57b0c7958a183cbea974
Your Name <you@example.com>
2021-09-23 15:33:57 -0600

    files

--- server.py
+++ server.py
@@ -3,52 +3,27 @@ from flask import Flask, render_template
 
 app= Flask( __name__)
 
-dictionarySports= {
-    'sport1':'Football',
-    'sport2':'Basketball',
-    'sport3':'Soccer',
-    'sport3':'Tennis'
-}
-listOfSports= [
-    {
-        'id':123,
-        'name':'Football'
-    },
-       {
-        id:456,
-        'name':'Basketball'
-    },   {
-        'id':789,
-        'name':'Soccer'
-    },
-    {
-        'id':555,
-        'name':'Tennis'
-    }
-]
 
-sportsList= ['Football', ' Basketball', 'Tennis', 'Soccer']
-
-@app.route('/home', methods=['GET'])
+@app.route('/play', methods=['GET'])
 def loadHome():
-    return render_template('index.html', firstName='Marco', sportsList=sportsList, listOfSports=listOfSports)
+    return render_template('index.html')
 
-@app.route('/sports', methods=['GET'] )
-def getSports():
-    print("This is executing my first endpoint in the server")
-    return dictionarySports
+#@app.route('/sports', methods=['GET'] )
+#def getSports():
+ #   print("This is executing my first endpoint in the server")
+  #  return dictionarySports
 
-@app.route('/sport/<id>', methods = ['GET'])
-def getSportById ( id ):
-    sportID= int(id)
-    for sport in listOfSports:
-        if sport['id']== sportID:
-            return "The sport of this id is "+sport['name']
+#@app.route('/sport/<id>', methods = ['GET'])
+#def getSportById ( id ):
+ #   sportID= int(id)
+  #  for sport in listOfSports:
+   #     if sport['id']== sportID:
+    #        return "The sport of this id is "+sport['name']
      
-        return "There is no sport with this id, try another one"
+     #   return "There is no sport with this id, try another one"
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+ #   app.run(debug=True)
     
